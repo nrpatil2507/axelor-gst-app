6210400adaec0dbb397efcf8ee49b057f0fe2d66
nrpatil2507 <nutanpatil2507@gmail.com>
2020-01-23 19:00:17 +0530

    add dashboard and add charts

--- modules/axelor-gst/src/main/java/com/axelor/gst/db/web/GstSequenceController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/web/GstSequenceController.java
@@ -1,44 +1,81 @@
 package com.axelor.gst.db.web;
 
+import com.axelor.gst.db.Invoice;
 import com.axelor.gst.db.Party;
 import com.axelor.gst.db.Sequence;
 import com.axelor.gst.db.repo.SequenceRepository;
-import com.axelor.gst.db.service.GstSequenceService;
 import com.axelor.inject.Beans;
 import com.axelor.meta.db.MetaModel;
 import com.axelor.meta.db.repo.MetaModelRepository;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
 
 public class GstSequenceController {
 
-  @Inject GstSequenceService sequenceService;
+	@Inject
+	private SequenceRepository sequenceRepo;
 
-  public void setPartySequence(ActionRequest request, ActionResponse response) {
+	@Transactional
+	public void setPartySequence(ActionRequest request, ActionResponse response) {
 
-    Party party = request.getContext().asType(Party.class);
-    Sequence sequence = new Sequence();
-    sequenceService = new GstSequenceService();
-    if (party.getReference() == null) {
-      MetaModel m = Beans.get(MetaModelRepository.class).findByName("Party");
- 
-      sequence = Beans.get(SequenceRepository.class).all().filter("self.model = ?", m).fetchOne();
-      int n = Integer.parseInt(sequence.getNextNumber());
+		Party party = request.getContext().asType(Party.class);
+		Sequence sequence = new Sequence();
 
-      String nextnumber = "" + n;
-      int size = nextnumber.length();
+		if (party.getReference() == null) {
 
-      for (int i = size; i <= sequence.getPadding(); i++) {
-        nextnumber = "0" + nextnumber;
-      }
-      String s = sequence.getPrefix() + nextnumber + sequence.getSufix();
-      n = n + 1;
-      String num = "" + n;
-      sequence.setNextNumber(num);
-      party.setReference(s);
-    }
-    response.setValues(party);
-    sequenceService.save(sequence);
-  }
+			MetaModel m = Beans.get(MetaModelRepository.class).findByName("Party");
+
+			sequence = Beans.get(SequenceRepository.class).all().filter("self.model = ?", m).fetchOne();
+			
+			if (sequence != null) {
+				int n = Integer.parseInt(sequence.getNextNumber());
+				String nextnumber = "" + n;
+				for (int i = nextnumber.length(); i <= sequence.getPadding(); i++) {
+					nextnumber = "0" + nextnumber;
+				}
+				n = n + 1;
+				sequence.setNextNumber(String.valueOf(n));
+				String partySequence = sequence.getPrefix() + nextnumber + sequence.getSufix();
+				party.setReference(partySequence);
+				sequenceRepo.persist(sequence);
+			}
+			response.setValues(party);
+		} else {
+			response.setFlash("sequence for invoice model was not generated");
+		}
+	}
+
+	@Transactional
+	public void setInvoiceSequence(ActionRequest request, ActionResponse response) {
+		Invoice invoice = request.getContext().asType(Invoice.class);
+		Sequence sequence = new Sequence();
+		String partySequence;
+		if (invoice.getReference() == null) {
+			MetaModel m = Beans.get(MetaModelRepository.class).findByName("Invoice");
+
+			sequence = Beans.get(SequenceRepository.class).all().filter("self.model = ?", m).fetchOne();
+			if (sequence != null) {
+				int n = Integer.parseInt(sequence.getNextNumber());
+
+				String nextnumber = "" + n;
+				for (int i = nextnumber.length(); i <= sequence.getPadding(); i++) {
+					nextnumber = "0" + nextnumber;
+				}
+				n = n + 1;
+				sequence.setNextNumber(String.valueOf(n));
+
+				if (sequence.getSufix() != null && nextnumber != null) {
+					partySequence = sequence.getPrefix() + nextnumber + sequence.getSufix();
+				} else
+					partySequence = sequence.getPrefix() + nextnumber;
+				invoice.setReference(partySequence);
+				sequenceRepo.persist(sequence);
+				response.setValues(invoice);
+			} else {
+				response.setFlash("sequence for invoice model was not generated");
+			}
+		}
+	}
 }
