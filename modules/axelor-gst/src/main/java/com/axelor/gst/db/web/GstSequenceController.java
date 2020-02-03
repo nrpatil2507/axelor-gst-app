077db2ac8f5e4b1e1e86a909cbe89846c5f2de72
nrpatil2507 <nutanpatil2507@gmail.com>
2020-02-03 11:34:48 +0530

    make changes in sequence

--- modules/axelor-gst/src/main/java/com/axelor/gst/db/web/GstSequenceController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/web/GstSequenceController.java
@@ -12,107 +12,98 @@ import com.google.inject.persist.Transactional;
 
 public class GstSequenceController {
 
-  @Inject private SequenceRepository sequenceRepo;
+	@Inject
+	private SequenceRepository sequenceRepo;
 
-  @Transactional
-  public void setSequenceData(ActionRequest request, ActionResponse response) {
-    Sequence sequence = request.getContext().asType(Sequence.class);
+	@Transactional
+	public void setSequenceData(ActionRequest request, ActionResponse response) {
+		Sequence sequence = request.getContext().asType(Sequence.class);
+		Sequence isSequence=sequenceRepo.find(sequence.getId());
+		if (isSequence == null) {
+			String suffix = sequence.getSufix();
+			String prefix = sequence.getPrefix();
+			String newNextNumber = null;
+			int padding = (int) sequence.getPadding();
+			String newPadding = "";
 
-    String suffix = sequence.getSufix();
-    String prefix = sequence.getPrefix();
-    String newNextNumber = null;
-    int padding = (int) sequence.getPadding();
-    String newPadding = "";
+			for (int i = 0; i < padding; i++) {
+				newPadding = newPadding + "0";
+			}
+			if (suffix == null) {
+				newNextNumber = prefix + newPadding;
+			} else {
+				newNextNumber = prefix + newPadding + suffix;
+			}
+			response.setValue("nextNumber", newNextNumber);
+		}
+	}
 
-    for (int i = 0; i < padding; i++) {
-      newPadding = newPadding + "0";
-    }
-    if (suffix == null) {
-      newNextNumber = prefix + newPadding;
-    } else {
-      newNextNumber = prefix + newPadding + suffix;
-    }
-    response.setValue("nextNumber", newNextNumber);
-  }
+	@Transactional
+	public void setPartySequence(ActionRequest request, ActionResponse response) {
 
-  @Transactional
-  public void setPartySequence(ActionRequest request, ActionResponse response) {
+		Party party = request.getContext().asType(Party.class);
+		Sequence sequence = new Sequence();
+		if (party.getReference() == null) {
+			sequence = Beans.get(SequenceRepository.class).all().filter("self.model.name = ?", "Party").fetchOne();
+			if (sequence != null) {
+				party.setReference(sequence.getNextNumber());
+				response.setValues(party);
 
-    Party party = request.getContext().asType(Party.class);
-    Sequence sequence = new Sequence();
-    if (party.getReference() == null) {
-      sequence =
-          Beans.get(SequenceRepository.class)
-              .all()
-              .filter("self.model.name = ?", "Party")
-              .fetchOne();
-      if(sequence!=null)
-      {
-    	party.setReference(sequence.getNextNumber());
-  		response.setValues(party);
-  		
-    	String suffix=sequence.getSufix();
-    	String prefix=sequence.getPrefix();
-    	String nextNumber;
-    	
-    	int paddingvalue = Integer.parseInt((sequence.getNextNumber().substring(prefix.length(), prefix.length()+sequence.getPadding())))+1;
-		String newpadding =Integer.toString(paddingvalue);
-		while(newpadding.length()<sequence.getPadding()){
-			newpadding="0"+newpadding;
-		}
-		if(suffix==null){
-			 nextNumber=prefix+newpadding;
-		}
-		else {
-			nextNumber=prefix+newpadding+suffix;
-		}
-		sequence.setNextNumber(nextNumber);
-		sequenceRepo.save(sequence);
-		
-      }
-      else
-      {
-    	 response.setError("sequence for party was not generated"); 
-      }
-    }
-  }
+				String suffix = sequence.getSufix();
+				String prefix = sequence.getPrefix();
+				String nextNumber;
 
-  @Transactional
-  public void setInvoiceSequence(ActionRequest request, ActionResponse response) {
-    Invoice invoice = request.getContext().asType(Invoice.class);
-    Sequence sequence = new Sequence();
-    if (invoice.getReference() == null) {
-      sequence =
-          Beans.get(SequenceRepository.class)
-              .all()
-              .filter("self.model.name = ?", "Invoice")
-              .fetchOne();
-      if(sequence!=null)
-      {
-    	invoice.setReference(sequence.getNextNumber());
-  		response.setValues(invoice);
-    	String suffix=sequence.getSufix();
-    	String prefix=sequence.getPrefix();
-    	String nextNumber;
-    	int paddingvalue = Integer.parseInt((sequence.getNextNumber().substring(prefix.length(),prefix.length()+sequence.getPadding())))+1;
-    	
-		String newpadding =Integer.toString(paddingvalue);
-		while(newpadding.length()<sequence.getPadding()){
-			newpadding="0"+newpadding;
+				int paddingvalue = Integer.parseInt(
+						(sequence.getNextNumber().substring(prefix.length(), prefix.length() + sequence.getPadding())))
+						+ 1;
+				String newpadding = Integer.toString(paddingvalue);
+				while (newpadding.length() < sequence.getPadding()) {
+					newpadding = "0" + newpadding;
+				}
+				if (suffix == null) {
+					nextNumber = prefix + newpadding;
+				} else {
+					nextNumber = prefix + newpadding + suffix;
+				}
+				sequence.setNextNumber(nextNumber);
+				sequenceRepo.save(sequence);
+
+			} else {
+				response.setError("sequence for party was not generated");
+			}
 		}
-		if(suffix==null){
-			 nextNumber=prefix+newpadding;
+	}
+
+	@Transactional
+	public void setInvoiceSequence(ActionRequest request, ActionResponse response) {
+		Invoice invoice = request.getContext().asType(Invoice.class);
+		Sequence sequence = new Sequence();
+		if (invoice.getReference() == null) {
+			sequence = Beans.get(SequenceRepository.class).all().filter("self.model.name = ?", "Invoice").fetchOne();
+			if (sequence != null) {
+				invoice.setReference(sequence.getNextNumber());
+				response.setValues(invoice);
+				String suffix = sequence.getSufix();
+				String prefix = sequence.getPrefix();
+				String nextNumber;
+				int paddingvalue = Integer.parseInt(
+						(sequence.getNextNumber().substring(prefix.length(), prefix.length() + sequence.getPadding())))
+						+ 1;
+
+				String newpadding = Integer.toString(paddingvalue);
+				while (newpadding.length() < sequence.getPadding()) {
+					newpadding = "0" + newpadding;
+				}
+				if (suffix == null) {
+					nextNumber = prefix + newpadding;
+				} else {
+					nextNumber = prefix + newpadding + suffix;
+				}
+				sequence.setNextNumber(nextNumber);
+				sequenceRepo.save(sequence);
+			} else {
+				response.setError("sequence for invoice was not generated");
+			}
 		}
-		else {
-			nextNumber=prefix+newpadding+suffix;
-		}
-		sequence.setNextNumber(nextNumber);
-		sequenceRepo.save(sequence);
-      }
-      else
-      {
-    	 response.setError("sequence for invoice was not generated"); 
-      }
-    }
-  }
+	}
 }
